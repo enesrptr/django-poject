1b11c5c110f3d8e3f20ae47ed36b30986d7c4bd3
enesrptr <enesmalikyildiz8@gmail.com>
2025-04-05 19:57:19 +0300

    continue templates and partials

--- courseapp/courses/views.py
+++ courseapp/courses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpResponseNotFound , HttpResponseRedirect
 from django.urls import reverse
+from datetime import date
 
 data = {
     "programlama" : "programlama kategorisine ait kurslar",
@@ -9,11 +10,47 @@ data = {
 
 }
 
-def index(request):
-    return render(request,'courses/index.html')
+db = {
+    "courses": [
+        {
+            "title" : "javascript kursu",
+            "description" : "javascript kurs aciklamasi",
+            "imageUrl" : "https://img-c.udemycdn.com/course/750x422/1662526_fc1c_3.jpg",
+            "slug" : "java-script-kursu",
+            "date" : date(2025,10,10)
+        },
 
-def kurslar(request):
-    return HttpResponse('kurslar')
+        {
+            "title" : "python kursu",
+            "description" : "python kurs aciklamasi",
+            "imageUrl" : "https://img-c.udemycdn.com/course/750x422/2463492_8344_3.jpg",
+            "slug" : "python-kursu",
+            "date" : date(2025,9,10)
+        },
+
+        {
+            "title" : "web gelistirme kursu",
+            "description" : "web gelistirme kurs aciklamasi",
+            "imageUrl" : "https://img-c.udemycdn.com/course/750x422/1258436_2dc3_4.jpg",
+            "slug" : "web-gelistirme-kursu",
+            "date" : date(2025,8,10)
+        }
+    ],
+    "categories" : [
+        {"id":1 ,"name": "programlama","slug":"programlama"},
+        {"id":2 ,"name": "web gelistirme","slug":"web-gelistirme"},
+        {"id":3 ,"name": "mobil uygulamalar","slug":"mobil-uygulamalar"}    
+    ]
+}
+
+def index(request):
+    kurslar = db["courses"]
+    kategoriler = db["categories"]
+
+    return render(request,'courses/index.html',{
+        'categories' : kategoriler,
+        'courses' : kurslar
+    })
 
 def kurslar_detay(request, kurs_adi):
     return HttpResponse(f'{kurs_adi} kurs detaylari')
@@ -22,7 +59,10 @@ def kurslar_detay(request, kurs_adi):
 def getCoursesByCategory(request, category_name):
     try:
         category_text = data[category_name]
-        return HttpResponse(category_text)
+        return render(request, 'courses/kurslar.html',{
+            'category' : category_name,
+            'category_text': category_text
+        })
     except:
         return HttpResponseNotFound("yanlis kategori secimi ")
 
