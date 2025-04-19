3d8b93f7032bdc038847781df7fbe31c8e649f61
enesrptr <enesmalikyildiz8@gmail.com>
2025-04-19 13:31:53 +0300

    GET forms

--- courseapp/courses/urls.py
+++ courseapp/courses/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.index),
+    path('',views.index, name="index"),
+    path('search',views.search, name="search"), #give a name to reach search and use it navbar
     path('<slug:slug>',views.details, name = "course_details"),
     path('category/<slug:slug>',views.getCoursesByCategory, name='courses_by_category'),
 ]
--- courseapp/courses/views.py
+++ courseapp/courses/views.py
@@ -4,13 +4,27 @@ from django.core.paginator import Paginator
 
 def index(request):
 
-    kurslar = Course.objects.filter(isActive = 1)
+    kurslar = Course.objects.filter(isActive = 1, isHome = 1)
     kategoriler = Category.objects.all()
 
 
     return render(request,'courses/index.html',{
         'categories' : kategoriler,
         'courses' : kurslar
+    })
+
+
+def search(request):
+    if "q" in request.GET and request.GET["q"] != "":
+        q = request.GET["q"]
+        kurslar = Course.objects.filter(isActive=1, title__contains = q).order_by("date")
+        kategoriler = Category.objects.all()
+    else:
+        return redirect("/kurslar")
+
+    return render(request, 'courses/search.html', {
+        'categories': kategoriler,
+        'courses': kurslar,
     })
 
 def details(request, slug):
@@ -32,14 +46,11 @@ def getCoursesByCategory(request, slug):
     kurslar = Course.objects.filter(categories__slug=slug, isActive=1).order_by("date")
     kategoriler = Category.objects.all()
 
-    paginator = Paginator(kurslar, 1)
+    paginator = Paginator(kurslar, 2)
     page = request.GET.get('page', 1)
     page_obj = paginator.get_page(page)
 
-    print(page_obj.paginator.count)
-    print(page_obj.paginator.num_pages)
-
-    return render(request, 'courses/index.html', {
+    return render(request, 'courses/list.html', {
         'categories': kategoriler,
         'page_obj': page_obj,
         'seciliKategori': slug
