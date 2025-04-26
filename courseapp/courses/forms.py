b087f60f4ca48ab7435bac5136350b4f5116023a
enesrptr <enesmalikyildiz8@gmail.com>
2025-04-26 14:03:45 +0300

    modelForm class

--- courseapp/courses/forms.py
+++ courseapp/courses/forms.py
@@ -1,13 +1,37 @@
 from django import forms
+from courses.models import Course
 
-class CourseCreateForm(forms.Form):
-    title = forms.CharField(
-        label="Kurs basligi",
-        required=True,
-        error_messages={"required":"kurs basligi girmelisiniz"},
-        widget=forms.TextInput(attrs={"class" : "form-control"})
-    )
+# class CourseCreateForm(forms.Form):
+#     title = forms.CharField(
+#         label="Kurs basligi",
+#         required=True,
+#         error_messages={"required":"kurs basligi girmelisiniz"},
+#         widget=forms.TextInput(attrs={"class" : "form-control"})
+#     )
 
-    description = forms.CharField(widget= forms.Textarea(attrs={"class" : "form-control"}))
-    imageUrl = forms.CharField(widget=forms.TextInput(attrs={"class" : "form-control"}))
-    slug = forms.CharField(widget=forms.TextInput(attrs={"class" : "form-control"}))
+#     description = forms.CharField(widget= forms.Textarea(attrs={"class" : "form-control"}))
+#     imageUrl = forms.CharField(widget=forms.TextInput(attrs={"class" : "form-control"}))
+#     slug = forms.CharField(widget=forms.TextInput(attrs={"class" : "form-control"}))
+
+class CourseCreateForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = ("title" , "description", "imageUrl", "slug")
+
+        widgets = {
+            "title" : forms.TextInput(attrs={"class":"form-control"}),
+            "description" : forms.Textarea(attrs={"class":"form-control"}),
+            "imageUrl" : forms.TextInput(attrs={"class":"form-control"}),
+            "slug" : forms.TextInput(attrs={"class":"form-control"}),
+        }
+        
+        error_messages = {
+            "title":{
+                "required":"kurs basligi girmelisiniz",
+                "max_length":"maksimum 50 karakter girmelisiniz",
+            },
+            "description":{
+                "required":"kurs aciklamasi gereklidir"
+            },
+        }
+        
