bf9516af93d61225bd300ba705164580b43fe42b
enesrptr <enesmalikyildiz8@gmail.com>
2025-04-26 13:15:47 +0300

    form template and errors

--- courseapp/courses/forms.py
+++ courseapp/courses/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 
 class CourseCreateForm(forms.Form):
-    title = forms.CharField()
-    description = forms.CharField(widget= forms.Textarea)
-    imageUrl = forms.CharField()
-    slug = forms.CharField()
+    title = forms.CharField(
+        label="Kurs basligi",
+        required=True,
+        error_messages={"required":"kurs basligi girmelisiniz"},
+        widget=forms.TextInput(attrs={"class" : "form-control"})
+    )
+
+    description = forms.CharField(widget= forms.Textarea(attrs={"class" : "form-control"}))
+    imageUrl = forms.CharField(widget=forms.TextInput(attrs={"class" : "form-control"}))
+    slug = forms.CharField(widget=forms.TextInput(attrs={"class" : "form-control"}))
