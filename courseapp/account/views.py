ad41566d43b5903c2eea144348fedcf214275bfc
enesrptr <enesmalikyildiz8@gmail.com>
2025-04-28 23:13:50 +0300

    user login

--- courseapp/account/views.py
+++ courseapp/account/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login
 
 def user_login(request):
-    return render(request, "account/login.html")
+    
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        
+        user = authenticate(request, username=username, password= password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("index")
+        else:
+            return render(request, "account/login.html", {"error":"username or password wrong"})
+    else:
+        return render(request, "account/login.html")
 
 def user_register(request):
     return render(request, "account/register.html")
