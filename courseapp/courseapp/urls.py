37d59b5f38a3940d4af3967fccb7895571194362
enesrptr <enesmalikyildiz8@gmail.com>
2025-05-03 00:20:38 +0300

    publosh things

--- courseapp/courseapp/urls.py
+++ courseapp/courseapp/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('account/', include('account.urls')),
     path('', include('pages.urls')),
     
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) \
++ static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
